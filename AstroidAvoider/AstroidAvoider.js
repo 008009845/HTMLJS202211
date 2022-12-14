fb47696055fb85658ab83f3baf69bb5e62ddd7b6
Ryan Blake <rrblake@email.neit.edu>
2022-12-14 13:05:26 -0500

    Add Fix

--- AstroidAvoider/AstroidAvoider.js
+++ AstroidAvoider/AstroidAvoider.js
@@ -212,7 +212,7 @@ function Asteroid(){
         ctx.save();
         ctx.beginPath();
         ctx.fillStyle=this.color;
-        ctx.arc(this.y, this.x, this.radius, 0, Math.PI*2, true);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
         ctx.closePath();
         ctx.fill();
         ctx.restore();
@@ -227,8 +227,8 @@ function PlayerShip(){
     this.height=20;
     this.up=false;
     this.down=false;
-    this.left=true;
-    this.right=true;
+    this.left=false;
+    this.right=false;
     this.vx=0;
     this.vy=0;
 
